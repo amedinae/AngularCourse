c90fa851286797a77c1c7bf9bbee74204baea666
amedinae <31489998+amedinae@users.noreply.github.com>
2023-08-04 22:06:17 +0200

    Share data between components

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  counter: number = 0;
+  @Input() counter: number = 0;
 }
