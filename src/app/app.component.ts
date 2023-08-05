ac7158c9e4f76cb4678f64e2fa4af623594f3be2
amedinae <31489998+amedinae@users.noreply.github.com>
2023-08-05 16:29:56 +0200

    Practice ngIf ngStyle ngClass and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showDetails: boolean = false;
+  clickLog: Date[] = [];
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+    this.clickLog.push(new Date());
+  }
 }
